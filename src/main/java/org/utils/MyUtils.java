c19d65e858853b16ad700b84634c05f095757366
JeraldChen <jeraldchenjunhan@gmail.com>
2023-03-03 20:37:12 +0800

    Added JUnit Test

--- src/main/java/org/utils/MyUtils.java
+++ src/main/java/org/utils/MyUtils.java
@@ -13,10 +13,14 @@ public class MyUtils {
 
         String str = "Hello|World|How are|you all";
 
-        List<String> stringParts = sp.splitString(str, "\\|");
+        try {
+            List<String> stringParts = sp.splitString(str, "\\|");
 
-        for (String part : stringParts) {
-            System.out.println(part);
+            for (String part : stringParts) {
+                System.out.println(part);
+            }
+        } catch (InvalidStringInputException ise) {
+            System.out.println("error here!");
         }
     }
 
